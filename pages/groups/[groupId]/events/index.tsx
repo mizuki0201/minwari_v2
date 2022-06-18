5ac18cd91b59fc67079c1cd4dd0705286b8e44fe
mizuki0201 <6m29uv7.tankss@gmail.com>
2022-06-18 21:10:24 +0900

    feat: Eventページの実装

---
+++ pages/groups/[groupId]/events/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { EventPage } from "../../../../components/pages/events/EventPage";
+
+const Event: NextPage = () => {
+  return <EventPage />;
+};
+
+export default Event;
